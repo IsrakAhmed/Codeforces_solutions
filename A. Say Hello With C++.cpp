8f487de417891578b2b7dec3d3be496b06ac9732
Israk Ahmed <israkahmed7@gmail.com>
2023-03-28 17:17:13 +0600

    A. Say Hello With C++ - solution added

---
+++ A. Say Hello With C++.cpp
@@ -0,0 +1,41 @@
+
+/*
+
+    Name        :   Israk Ahmed
+    E-Mail      :   israkahmed7@gmail.com
+    Github      :   github.com/IsrakAhmed
+    Institution :   University of Rajshahi
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define asort(v) sort((v).begin(),(v).end())        // Ascending Order Sort
+#define dsort(v) sort((v).rbegin(),(v).rend())     // Descending Order Sort
+#define pb push_back
+#define sum(v) accumulate(v.begin(), v.end(), 0)
+#define gcd __gcd
+#define toLower(s) transform((s).begin(),(s).end(),(s).begin(),::tolower)       //toLower(s);
+#define toUpper(s) transform((s).begin(),(s).end(),(s).begin(),::toupper)       //toUpper(s);
+#define rmvChar(s,c) (s).erase(remove((s).begin(), (s).end(), c), (s).end())    //remove specific character from string
+#define rmvDuplicates(x) (x).erase(unique((x).begin(), (x).end()), (x).end());  //works only for sorted string
+#define all(x) (x).begin(),(x).end()
+
+ll random(ll low, ll high) {
+    // Generate random number
+    return rand() % (high - low + 1) + low;
+}
+
+void solution(){
+    string s;
+    cin >> s;
+    cout << "Hello, " << s << endl;
+}
+
+int main(){
+    solution();
+    return 0;
+}
