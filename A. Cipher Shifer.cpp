5f0a55eb903834bb1e67a6b95c919432333613b6
Israk Ahmed <israkahmed7@gmail.com>
2023-06-26 19:23:00 +0600

    A. Cipher Shifer - solution added

---
+++ A. Cipher Shifer.cpp
@@ -0,0 +1,68 @@
+
+/*
+
+    Name        :   Israk Ahmed
+    E-Mail      :   israkahmed7@gmail.com
+    Github      :   github.com/IsrakAhmed
+    Institution :   University of Rajshahi
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define asort(v) sort((v).begin(),(v).end())        // Ascending Order Sort
+#define dsort(v) sort((v).rbegin(),(v).rend())     // Descending Order Sort
+#define pb push_back
+#define sum(v) accumulate(v.begin(), v.end(), 0)
+#define gcd __gcd
+#define toLower(s) transform((s).begin(),(s).end(),(s).begin(),::tolower)       //toLower(s);
+#define toUpper(s) transform((s).begin(),(s).end(),(s).begin(),::toupper)       //toUpper(s);
+#define rmvChar(s,c) (s).erase(remove((s).begin(), (s).end(), c), (s).end())    //remove specific character from string
+#define rmvDuplicates(x) (x).erase(unique((x).begin(), (x).end()), (x).end());  //works only for sorted string
+#define all(x) (x).begin(),(x).end()
+#define nl "\n"
+
+ll random(ll low, ll high)
+{
+    // Generate random number
+    return rand() % (high - low + 1) + low;
+}
+
+void solution()
+{
+    ll t;
+    cin >> t;
+
+    while(t--)
+    {
+        ll n;
+        string s,finalS;
+
+        cin >> n >> s;
+
+        ll i = 0;
+
+        while(i < n){
+            finalS.pb(s[i]);
+            ll j;
+            for(j = i + 1; j < n; j++){
+                if(s[j] == s[i]){
+                    break;
+                }
+            }
+            i = j + 1;
+        }
+        cout << finalS << endl;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solution();
+    return 0;
+}
