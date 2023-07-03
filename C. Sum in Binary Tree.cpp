40a21f984e3824770f2cd0497188efe907146a47
Israk Ahmed <israkahmed7@gmail.com>
2023-07-03 15:47:14 +0600

    C. Sum in Binary Tree - solution added

---
+++ C. Sum in Binary Tree.cpp
@@ -0,0 +1,55 @@
+
+/*
+
+    Name        :   Israk Ahmed
+    E-Mail      :   israkahmed7@gmail.com
+    Github      :   github.com/IsrakAhmed
+    Institution :   University of Rajshahi
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define asort(v) sort((v).begin(),(v).end())        // Ascending Order Sort
+#define dsort(v) sort((v).rbegin(),(v).rend())     // Descending Order Sort
+#define pb push_back
+#define sum(v) accumulate(v.begin(), v.end(), 0)
+#define gcd __gcd
+#define toLower(s) transform((s).begin(),(s).end(),(s).begin(),::tolower)       //toLower(s);
+#define toUpper(s) transform((s).begin(),(s).end(),(s).begin(),::toupper)       //toUpper(s);
+#define rmvChar(s,c) (s).erase(remove((s).begin(), (s).end(), c), (s).end())    //remove specific character from string
+#define rmvDuplicates(x) (x).erase(unique((x).begin(), (x).end()), (x).end());  //works only for sorted string
+#define all(x) (x).begin(),(x).end()
+#define nl "\n"
+
+void solution()
+{
+    ll t;
+    cin >> t;
+
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+
+        ll sum = 0;
+
+        while(n){
+            sum += n;
+            n /= 2;
+        }
+
+        printf("%lld\n",sum);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solution();
+    return 0;
+}
