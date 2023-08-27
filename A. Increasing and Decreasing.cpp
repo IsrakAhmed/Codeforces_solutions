b1281a22a6e2ad2db67be1cce40b6f792bbb4126
IsrakAhmed <israkahmed7@gmail.com>
2023-08-27 19:41:33 +0600

    A. Increasing and Decreasing - solution added

---
+++ A. Increasing and Decreasing.cpp
@@ -0,0 +1,67 @@
+
+/*
+
+    Name        :   Israk Ahmed
+    E-Mail      :   israkahmed7@gmail.com
+    Github      :   github.com/IsrakAhmed
+    Institution :   University of Rajshahi
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define asort(v) sort((v).begin(),(v).end())        // Ascending Order Sort
+#define dsort(v) sort((v).rbegin(),(v).rend())     // Descending Order Sort
+#define pb push_back
+#define sum(v) accumulate(v.begin(), v.end(), 0)
+#define gcd __gcd
+#define toLower(s) transform((s).begin(),(s).end(),(s).begin(),::tolower)       //toLower(s);
+#define toUpper(s) transform((s).begin(),(s).end(),(s).begin(),::toupper)       //toUpper(s);
+#define rmvChar(s,c) (s).erase(remove((s).begin(), (s).end(), c), (s).end())    //remove specific character from string
+#define rmvDuplicates(x) (x).erase(unique((x).begin(), (x).end()), (x).end());  //works only for sorted string
+#define all(x) (x).begin(),(x).end()
+#define nl "\n"
+
+void solution()
+{
+    ll t;
+    cin >> t;
+
+    while(t--)
+    {
+        ll x,y,n;
+        cin >> x >> y >> n;
+
+        ll arr[n];
+        ll j = 1;
+
+        arr[n - 1] = y;
+        for(ll i = n - 2; i >= 0; i--){
+            arr[i] = arr[i + 1] - j;
+            j++;
+        }
+
+        if(arr[0] < x){
+            cout << -1 << endl;
+        }
+        else{
+            cout << x;
+            for(ll i = 1; i < n; i++){
+                cout << " " << arr[i];
+            }
+            cout << endl;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solution();
+    return 0;
+}
