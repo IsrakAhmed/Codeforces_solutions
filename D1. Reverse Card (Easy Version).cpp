22ea437453267d94ae6e5a2a75c7a7b32cba0269
IsrakAhmed <israkahmed7@gmail.com>
2024-05-01 00:13:26 +0600

    D1. Reverse Card (Easy Version) - solution added

---
+++ D1. Reverse Card (Easy Version).cpp
@@ -0,0 +1,62 @@
+
+/*
+
+    Name        :   Israk Ahmed
+    E-Mail      :   israkahmed7@gmail.com
+    Github      :   github.com/IsrakAhmed
+    Institution :   University of Rajshahi
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll unsigned long long
+#define asort(v) sort((v).begin(),(v).end())        // Ascending Order Sort
+#define dsort(v) sort((v).rbegin(),(v).rend())     // Descending Order Sort
+#define pb push_back
+#define sum(v) accumulate(v.begin(), v.end(), 0)
+#define gcd __gcd
+#define toLower(s) transform((s).begin(),(s).end(),(s).begin(),::tolower)       //toLower(s);
+#define toUpper(s) transform((s).begin(),(s).end(),(s).begin(),::toupper)       //toUpper(s);
+#define rmvChar(s,c) (s).erase(remove((s).begin(), (s).end(), c), (s).end())    //remove specific character from string
+#define rmvDuplicates(x) (x).erase(unique((x).begin(), (x).end()), (x).end());  //works only for sorted string
+#define all(x) (x).begin(),(x).end()
+#define endl "\n"
+
+void solution()
+{
+    ll t;
+    cin >> t;
+
+    while(t--)
+    {
+        ll n,m;
+        cin >> n >> m;
+
+        ll cnt = 0;
+
+        for(ll b = 1; b <= m; b++)
+        {
+            for(ll a = b; a <= n; a += b)
+            {
+                if((a + b) % (b * gcd(a,b)) == 0)
+                {
+                    cnt++;
+                }
+            }
+        }
+
+        printf("%u\n",cnt);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solution();
+    return 0;
+}
