9a8d2d5851dbe65f75a495af027079340fa7ad56
IsrakAhmed <israkahmed7@gmail.com>
2024-06-13 00:38:05 +0600

    D. Manhattan Circle - solution added

---
+++ D/D. Manhattan Circle.cpp
@@ -0,0 +1,97 @@
+
+/*
+
+    Name        :   Israk Ahmed
+    E-Mail      :   israkahmed7@gmail.com
+    Github      :   github.com/IsrakAhmed
+    LinkedIn    :   linkedin.com/in/israkahmed
+    Institution :   University of Rajshahi
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define asort(v) sort((v).begin(),(v).end())        //  Ascending Order Sort
+#define dsort(v) sort((v).rbegin(),(v).rend())     //   Descending Order Sort
+#define pb push_back
+#define sum(v) accumulate(v.begin(), v.end(), 0)
+#define rotates(v,i) rotate(v.begin(), v.begin() + i, v.end())       //  moves first i items to last
+#define isSorted(v) is_sorted(v.begin(), v.end())                    //  checks if the vector is sorted or not
+#define gcd __gcd
+#define toLower(s) transform((s).begin(),(s).end(),(s).begin(),::tolower)       //  toLower(s);
+#define toUpper(s) transform((s).begin(),(s).end(),(s).begin(),::toupper)       //  toUpper(s);
+#define rmvChar(s,c) (s).erase(remove((s).begin(), (s).end(), c), (s).end())    //  remove specific character from string
+#define rmvDuplicates(x) (x).erase(unique((x).begin(), (x).end()), (x).end());  //  works only for sorted string or vector
+#define all(x) (x).begin(),(x).end()
+#define endl "\n"
+#define merge(v1,v2,m) merge(v1.begin(), v1.end(), v2.begin(), v2.end(),m.begin());     //  merge two vectors
+#define reverseS(s) reverse(s.begin(), s.end())     //  reverse the string
+#define nSum(n) (n * (n + 1)) / 2                   //  sum of first N positive integers
+#define nmSum(n,m) ((n + m) * (m - n + 1)) / 2      //  sum of positive integers from N to M (N <= M)
+
+
+void solution()
+{
+    ll t;
+    cin >> t;
+
+    while(t--)
+    {
+        ll n,m;
+        cin >> n >> m;
+
+        vector <string> v;
+
+        map <ll,ll> mm;
+
+        for(ll i = 0; i < n; i++)
+        {
+            string s;
+            cin >> s;
+
+            v.pb(s);
+
+            rmvChar(s,'#');
+
+            ll numofhash = m - s.size();
+
+            mm[i] = numofhash;
+        }
+
+        ll maxm = mm[0];
+        ll maxnum = 0;
+
+        for(auto &i : mm)
+        {
+            if(i.second > maxm)
+            {
+                maxm = i.second;
+                maxnum = i.first;
+            }
+        }
+
+        ll adds = maxm / 2;
+
+        string s = v[maxnum];
+
+        for(ll i = 0; i < s.size(); i++){
+            if(s[i] == '#'){
+                printf("%lld %lld\n",maxnum + 1, (i + adds) + 1);
+                break;
+            }
+        }
+
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solution();
+    return 0;
+}
