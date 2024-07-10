b4727b135fda73b44430c294af6ffce9f4071a87
IsrakAhmed <israkahmed7@gmail.com>
2024-07-10 12:58:20 +0600

    A. Remove Duplicates - solution added

---
+++ A/A. Remove Duplicates.cpp
@@ -0,0 +1,122 @@
+/*
+
+            Bismillah Hir Rehmanir Rahim
+            Allah knows best
+
+*/
+
+/*
+
+    Name        :   Israk Ahmed
+    E-Mail      :   israkahmed7@gmail.com
+    Github      :   github.com/IsrakAhmed
+    LinkedIn    :   linkedin.com/in/israkahmed
+    Institution :   University of Rajshahi
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define pi 3.141592653589793
+#define f(i, a, n) for (ll i = a; i < n; i++)
+#define asort(v) sort((v).begin(),(v).end())        //  Ascending Order Sort
+#define dsort(v) sort((v).rbegin(),(v).rend())     //   Descending Order Sort
+#define pb push_back
+#define mp make_pair
+#define sum(v) accumulate(v.begin(), v.end(), 0)
+#define rotates(v,i) rotate(v.begin(), v.begin() + i, v.end())       //  moves first i items to last
+#define isSorted(v) is_sorted(v.begin(), v.end())                    //  checks if the vector is sorted or not
+#define gcd __gcd
+#define toLower(s) transform((s).begin(),(s).end(),(s).begin(),::tolower)       //  toLower(s);
+#define toUpper(s) transform((s).begin(),(s).end(),(s).begin(),::toupper)       //  toUpper(s);
+#define rmvChar(s,c) (s).erase(remove((s).begin(), (s).end(), c), (s).end())    //  remove specific character from string
+#define rmvDuplicates(x) (x).erase(unique((x).begin(), (x).end()), (x).end());  //  works only for sorted string or vector
+#define all(x) (x).begin(),(x).end()
+#define endl "\n"
+#define merge(v1,v2,m) merge(v1.begin(), v1.end(), v2.begin(), v2.end(),m.begin());     //  merge two vectors
+#define reverseS(s) reverse(s.begin(), s.end())     //  reverse the string
+#define nSum(n) (n * (n + 1)) / 2                   //  sum of first N positive integers
+#define nmSum(n,m) ((n + m) * (m - n + 1)) / 2      //  sum of positive integers from N to M (N <= M)
+
+const int dx[] = {0, -1, 0, 1, -1, -1, 1, 1};
+const int dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+
+ll finds(vector <ll> v, ll target)
+{
+
+    vector <ll>::iterator i;
+
+    i = find(v.begin(), v.end(), target);
+
+    if (i != v.end())
+    {
+        //return (i - v.begin()) + 1;   // returns position
+
+        return 1;   // returns 1 as true
+    }
+
+    //return -1;  // returns position as not  found
+
+    return 0;   // returns 0 as false
+}
+
+
+void solution()
+{
+    ll n;
+    cin >> n;
+
+    vector <ll> v(n);
+
+    set <ll> s;
+
+    ll i = 0;
+
+    f(i, 0, n){
+        cin >> v[i];
+
+        s.insert(v[i]);
+    }
+
+    printf("%lld\n",s.size());
+
+    vector <ll> st(s.size());
+
+    ll x = s.size() - 1;
+
+    for(ll i = n - 1; i >= 0; i--){
+        if(v[i] != 0){
+            st[x] = v[i];
+            x--;
+
+            for(ll j = i - 1; j >= 0; j--){
+                if(v[i] == v[j]){
+                    v[j] = 0;
+                }
+            }
+        }
+    }
+
+
+    f(i, 0, s.size()){
+        printf("%lld ",st[i]);
+    }
+
+    printf("\n");
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solution();
+    return 0;
+}
